f5a943d402a4886774dd88821a01cee5b2ed949a
Elsa Catoire <elsa.catoire@gmail.com>
2023-05-19 10:37:02 +0200

    add an error for the function that moves

--- sorting.js
+++ sorting.js
@@ -10,15 +10,19 @@ function createRandomList(howManyNb) {
 }
 
 function moveElement(srcList, indexFrom, indexTo) {
-    elementToMove = srcList[indexFrom]
-    let modifiedList = []
-    let before = srcList.slice(0, indexFrom)
-    let between = srcList.slice(indexFrom + 1, indexTo)
-    modifiedList = before.concat(between)
-    modifiedList.push(elementToMove)
-    let after = srcList.slice(indexTo)
-    modifiedList = modifiedList.concat(after)
-    return modifiedList
+    if (indexFrom < indexTo) {
+        elementToMove = srcList[indexFrom]
+        let modifiedList = []
+        let before = srcList.slice(0, indexFrom)
+        let between = srcList.slice(indexFrom + 1, indexTo)
+        modifiedList = before.concat(between)
+        modifiedList.push(elementToMove)
+        let after = srcList.slice(indexTo)
+        modifiedList = modifiedList.concat(after)
+        return modifiedList
+    } else {
+        return console.log('error : order of exchange not supported');
+    }
 }
 
 function sortList(ListToSort) {
@@ -33,4 +37,4 @@ function sortList(ListToSort) {
 let randomList = createRandomList(4)
 console.log(randomList);
 
-console.log(moveElement([1, 2, 3, 4], 0, 2));
+console.log(moveElement([1, 2, 3, 4, 5, 6], 1, 3));
