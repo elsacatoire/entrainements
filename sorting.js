80d9e039c0d7a372849a5f541eb335c53c958559
Elsa Catoire <elsa.catoire@gmail.com>
2023-05-19 10:33:20 +0200

    fcontion move lement ok pour déplacempent positif

--- sorting.js
+++ sorting.js
@@ -9,7 +9,28 @@ function createRandomList(howManyNb) {
     return finalList
 }
 
+function moveElement(srcList, indexFrom, indexTo) {
+    elementToMove = srcList[indexFrom]
+    let modifiedList = []
+    let before = srcList.slice(0, indexFrom)
+    let between = srcList.slice(indexFrom + 1, indexTo)
+    modifiedList = before.concat(between)
+    modifiedList.push(elementToMove)
+    let after = srcList.slice(indexTo)
+    modifiedList = modifiedList.concat(after)
+    return modifiedList
+}
+
+function sortList(ListToSort) {
+    for (let i = 0; i < ListToSort.length; i++) {
+        const element = array[i];
+
+    }
+}
+
 
 // main application
 let randomList = createRandomList(4)
 console.log(randomList);
+
+console.log(moveElement([1, 2, 3, 4], 0, 2));
