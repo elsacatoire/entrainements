adb0699f04501e446681c0ae8983314c92dcb63a
Elsa Catoire <elsa.catoire@gmail.com>
2023-06-13 21:04:46 +0200

    start poker

---
+++ poker-python/main.py
@@ -0,0 +1,20 @@
+import random
+
+
+# poker game by Esa
+def main():
+    class Card(object):
+        def __init__(self, value, suit):
+            self.value = value
+            self.suit = suit
+
+    class StandardDeck(list):
+        def __init__(self):
+
+
+
+
+# We run thongs here
+if __name__ == '__main__':
+    print("hi ! Welcome to my game")
+
