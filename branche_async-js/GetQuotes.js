70fbb2369ec51b7a76527860d3b03a24a8da3673
Elsa Catoire <elsa.catoire@gmail.com>
2023-06-19 15:45:54 +0200

    afficher une positiv quote ok

--- branche_async-js/GetQuotes.js
+++ branche_async-js/GetQuotes.js
@@ -1,9 +1,13 @@
 
 // Api qui va me fournir mes quotes
 
-const positivQuotes = 'https://www.affirmations.dev/'
+async function fetchPositivQuote() {
+    const positivQuotes = 'https://www.affirmations.dev/'
 
-const responseQuote = await fetch(positivQuotes)
-const dataQuote = await responseQuote.json()
+    const responseQuote = await fetch(positivQuotes)
+    const dataQuote = await responseQuote.json()
 
-console.log(dataQuote);
+    console.log(dataQuote.affirmation);
+}
+
+fetchPositivQuote()
