4612ab9f8d10bd4afe2ac715fc498387cfd79d8e
Elsa Catoire <elsa.catoire@gmail.com>
2023-06-21 15:39:54 +0200

    shell of the diffrent routes

--- API-nodeJS/routes/subscribers.js
+++ API-nodeJS/routes/subscribers.js
@@ -2,10 +2,25 @@ const express = require('express')
 const router = express.Router()
 
 // Getting all
+router.get('/', (req, res) => {
+
+})
 
 // Getting one
+router.get('/:id', (req, res) => {
+
+})
 // Creating one
+router.post('/', (req, res) => {
+
+})
 // Updating one
+router.patch('/', (req, res) => {
+
+})
 // Deleting one
+router.delete('/:id', (req, res) => {
+
+})
 
 module.exports = router
