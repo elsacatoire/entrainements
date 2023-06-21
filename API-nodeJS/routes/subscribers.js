cde4968df5f74acbf7884544eba4868fa26dcd6e
Elsa Catoire <elsa.catoire@gmail.com>
2023-06-21 15:52:46 +0200

    add subscriber schema

--- API-nodeJS/routes/subscribers.js
+++ API-nodeJS/routes/subscribers.js
@@ -3,11 +3,13 @@ const router = express.Router()
 
 // Getting all
 router.get('/', (req, res) => {
+    res.send('Hello World')
 
 })
 
 // Getting one
 router.get('/:id', (req, res) => {
+    res.send(req.params.id)
 
 })
 // Creating one
