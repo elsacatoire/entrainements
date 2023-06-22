9f9c371e016e68f554aa140859293916078c9d93
Elsa Catoire <elsa.catoire@gmail.com>
2023-06-22 11:04:35 +0200

    route GET vers JSON all parkinks

--- API-node2/index.js
+++ API-node2/index.js
@@ -1,10 +1,14 @@
+
+
 // création du serveur Express
 const express = require('express')
 const app = express()
+// importer les données
+const parkings = require('./parkings.json')
 
-// définir la route GET
+// définir la route GET pour tous les parkings
 app.get('/parkings', (req,res) => {
-    res.send("Liste des parkings")
+    res.status(200).json(parkings)
 })
 app.listen(8080, () => {
     console.log("Serveur à l'écoute")
