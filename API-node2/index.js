a68b017e394e2243b5b2c85e08215ee8f58a3f50
Elsa Catoire <elsa.catoire@gmail.com>
2023-06-22 11:31:58 +0200

    comment et prepa routes pour reservations

--- API-node2/index.js
+++ API-node2/index.js
@@ -1,19 +1,29 @@
-// création du serveur Express
+/* 
+elsa_des_bois création un API avec NodeJS
+22 06 2023 Ada Tech School
+on commence par créer un serveur local host
+*/
+
+/* création du serveur Express */
 const express = require('express')
 const app = express()
 
-// importer les données
+/* IMPORT des fichiers */
+// importer les données de la ressource parkings
 const parkings = require('./parkings.json')
-
+// importer les données de la ressources reservations
+const reservations = require('./reservations')
 // Middleware
 app.use(express.json())
 
-// définir la route GET pour tous les parkings
+
+/* --- DEFINITION des routes pour la ressource parkings --- */
+// définir la route GET pour obtenir tous les parkings
 app.get('/parkings', (req,res) => {
     res.status(200).json(parkings)
 })
 
-// définir la route GET pour un parking avec son id
+// définir la route GET pour obtenir un parking grace à son id
 app.get('/parkings/:id', (req,res) => {
     const id = parseInt(req.params.id)
     const parking = parkings.find(parking => parking.id === id)
@@ -43,7 +53,20 @@ app.delete('/parkings/:id', (req,res) => {
     res.status(200).json(parkings)})
 
 
-// quand le serveur se lance
+    /* --- DEFINITION des routes pour la ressource reservations --- */
+// définir la route GET pour tous les parkings
+
+// définir la route GET pour un parking avec son id
+
+// définir la route POST pour céer un nouveau parking
+
+// définir la route PUT pour modifier un parking
+
+// définir la route DELETE pour supprimer un parking
+
+
+
+/* --- Quand le serveur se lance --- */
 app.listen(8080, () => {
     console.log("Serveur à l'écoute")
 })
