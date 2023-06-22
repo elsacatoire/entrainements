6f91db57d333d77a1fe8f880801252259ccb2268
Elsa Catoire <elsa.catoire@gmail.com>
2023-06-22 11:08:47 +0200

    ajout de la route GET avec id

--- API-node2/index.js
+++ API-node2/index.js
@@ -10,6 +10,15 @@ const parkings = require('./parkings.json')
 app.get('/parkings', (req,res) => {
     res.status(200).json(parkings)
 })
+
+// définir la route GET pour un parking avec son id
+app.get('/parkings/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    const parking = parkings.find(parking => parking.id === id)
+    res.status(200).json(parking)
+})
+
+// quand le serveur se lance
 app.listen(8080, () => {
     console.log("Serveur à l'écoute")
 })
