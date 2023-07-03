adbf3b293514fb83ec1d36ec8c454268b4d3cb10
Elsa Catoire <elsa.catoire@gmail.com>
2023-07-03 16:37:09 +0200

    dynamice add todos

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -1,32 +1,59 @@
+import { useState } from "react"
 import ('./styles.css')
 
 export default function App(params) {
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault() //prevent my page to refreshing
+
+    // in order to re render you need to pass a function to change the state
+    setTodos((currentTodos) => {
+      return[
+        ...todos, 
+        { id: crypto.randomUUID, title: newItem, completed: false},
+      ]
+    })
+    setNewItem("")
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+        return todo
+      })
+    })
+    
+  }
+  
   return (
   <>
-  <form className="new-item-form">
+  <form onSubmit={handleSubmit} className="new-item-form">
     <div className="form-row">
       <label htmlFor="item">New label</label>
-      <input type="text" id="item"></input>
-    </div>
+      <input value={newItem} 
+      onChange={e => setNewItem(e.target.value)} 
+      type="text" 
+      id="item"></input> 
+    </div> 
     <button className="btn">Add</button>
   </form>
   <h1 className="header">Todo List</h1>
   <ul className="list">
-    <li>
+    {todos.map(todo => {
+      return <li key={todo.id}>
       <label>
-      <input type="checkbox"></input>
-      item 1
+        <input type="checkbox" checked={todo.completed}
+        onChange={e => toggleTodo(todo.id, e.target.checked)}/>
+        {todo.title}
       </label>
       <button className="btn btn-danger">Delete</button>      
     </li>
-    <li>
-      <label>
-      <input type="checkbox"></input>
-      item 2
-      </label>
-      <button className="btn btn-danger">Delete</button>      
-    </li>
-    
+    })}
 
   </ul>
   </>
