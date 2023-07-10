60879df20796e8927e0c4ddfe1431bacd9c05300
Elsa Catoire <elsa.catoire@gmail.com>
2023-07-10 10:48:06 +0200

    setup

---
+++ LeapYears_TDD-JS/leapYears.js
@@ -0,0 +1,18 @@
+
+//    /\  /\ /\ /\                              /\  /\  /\
+//   /  \/  \  /  \ elsa_des_bois              /  \/  \/  \
+//  /   /    \/    \    fonctions principales /    \   \   \
+//    ||  || || ||                              ||  ||  ||
+//                      **Leap years TDD**
+
+// ==> importer les fonctions dont on a besoin <==
+
+// ==> déclaration des fonctions <==
+
+//    /\  /\ /\ /\                              /\  /\  /\
+//   /  \/  \  /  \      Le code s'exécute     /  \/  \/  \
+//  /   /    \/    \            ICI  :        /    \   \   \
+//    ||  || || ||                              ||  ||  ||
+
+
+// ==> export des modules <===
---
+++ LeapYears_TDD-JS/leapYears.test.js
@@ -0,0 +1,19 @@
+//    /\  /\ /\ /\                              /\  /\  /\
+//   /  \/  \  /  \ elsa_des_bois              /  \/  \/  \
+//  /   /    \/    \    tests unitaire        /    \   \   \
+//    ||  || || ||                              ||  ||  ||
+//                      **Leap years TDD**
+
+// ==> importer les fonctions dont on a besoin <==
+
+// ==> déclaration des fonctions de test <==
+
+
+
+//    /\  /\ /\ /\                              /\  /\  /\
+//   /  \/  \  /  \      Le code s'exécute     /  \/  \/  \
+//  /   /    \/    \            ICI  :        /    \   \   \
+//    ||  || || ||                              ||  ||  ||
+
+
+// ==> export des modules <===
