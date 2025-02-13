1133c2f6593c276755640ed5b7550e73682b6f08
Elsa Catoire <elsa.catoire@gmail.com>
2025-02-13 23:34:23 +0100

    refactor sumEven function and tests to use TypeScript with improved error handling

--- algo/ts_exercises/sumEven.test.ts
+++ algo/ts_exercises/sumEven.test.ts
@@ -1,15 +1,15 @@
-const sumEven = require('./sumEven')
+import { sumEven } from "./sumEven"
 
 test('Normal number array', () =>  {
     expect(sumEven([1, 2, 3, 4, 5, 6])).toBe(12)
 })
 
 test('Not an array', () =>  {
-    expect(sumEven("coucou")).toBe('Not an array')
+    expect(() => sumEven("coucou" as unknown as Array<number>)).toThrow('Not an array')
 })
 
 test('Array with strings', () =>  {
-    expect(sumEven([1, 'chat', 3, 4, 5, 6])).toBe(10)
+    expect(sumEven([1, 'chat' as unknown as number, 3, 4, 5, 6])).toBe(10)
 })
 
 test('Empty string', () =>  {
--- algo/ts_exercises/sumEven.ts
+++ algo/ts_exercises/sumEven.ts
@@ -1,7 +1,4 @@
-function sumEven(arr) {
-    if (!Array.isArray(arr)) return 'Not an array';
+export function sumEven(arr: Array<number>): number {
+    if (!Array.isArray(arr)) throw new Error ('Not an array');
     return arr.filter((num) => num % 2 === 0).reduce((acc, num) => acc + num, 0);
 }
-  
-module.exports = sumEven
-  
