eef0d5528de6e35daf95981d2b11a0116d87d119
Elsa Catoire <elsa.catoire@gmail.com>
2025-02-14 12:13:07 +0100

    refactor capitalizeWords function and tests to improve input handling and add edge case tests

--- algo/l2_ts_ex/capitalizeWords.test.ts
+++ algo/l2_ts_ex/capitalizeWords.test.ts
@@ -8,6 +8,14 @@ test('a sentence', () =>{
     expect(capitalizeWords("hello world")).toBe("Hello World")
 })
 
+test('Extra spaces', () =>{
+    expect(capitalizeWords("   hello    world   ")).toBe("Hello World")
+});
+
 test('Invalid entry', () => {
     expect(() => capitalizeWords(4567 as unknown as string)).toThrow("Invalid text")
+})
+
+test('Empty text', () => {
+    expect(() => capitalizeWords('')).toThrow("Invalid text")
 })
--- algo/l2_ts_ex/capitalizeWords.ts
+++ algo/l2_ts_ex/capitalizeWords.ts
@@ -1,5 +1,9 @@
 export const  capitalizeWords = (text: string): string => {
     if (typeof text !== 'string' || text.length < 1) throw new Error ("Invalid text")
-    const capitalizeText = text.split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
-    return capitalizeText
+
+    return text
+    .trim()
+    .split(/\s+/)
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
 }
