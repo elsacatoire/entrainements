af06da1908fff3a86350242e3b0146d4e7db9aa1
Elsa Catoire <elsa.catoire@gmail.com>
2025-02-14 12:27:32 +0100

    refactor isAnagram function and tests to improve input validation and handle edge cases

--- algo/l2_ts_ex/isAnagram.test.ts
+++ algo/l2_ts_ex/isAnagram.test.ts
@@ -4,6 +4,14 @@ test("A valid anagram", () => {
     expect(isAnagram(['car', 'arc'])).toBeTruthy()
 })
 
+test("An anagram with different cases", () => {
+    expect(isAnagram(['Car', 'aRc'])).toBeTruthy()
+});
+
+test("An anagram with spaces", () => {
+    expect(isAnagram([' list en', '  silent'])).toBeTruthy()
+});
+
 test("An invalid anagram", () => {
     expect(isAnagram(['tree', 'love'])).toBeFalsy()
 })
@@ -12,6 +20,10 @@ test("Not an array", () => {
     expect(() => isAnagram('mouhahah' as unknown as Array<string>)).toThrow('Invalid entry')
 })
 
-test("Invali array", () => {
+test("Invalid array length", () => {
     expect(() => isAnagram([])).toThrow('Invalid entry')
 })
+
+test("Empty strings", () => {
+    expect(() => isAnagram(['', ''])).toThrow('Invalid entry')
+});
--- algo/l2_ts_ex/isAnagram.ts
+++ algo/l2_ts_ex/isAnagram.ts
@@ -1,5 +1,14 @@
 export  const isAnagram = (words: Array<string>): boolean => {
-    if (!Array.isArray(words) || words.length !== 2) throw new Error("Invalid entry")
-    const sortedWords = words.map((word) => word.split('').sort().join(''));
-    return sortedWords[0] === sortedWords[1];
+    if (!Array.isArray(words) || words.length !== 2 || words.some(w => typeof w !== 'string' || w.trim() === '') ) {
+        throw new Error("Invalid entry")
+    }
+ 
+    const normalize = (word: string) => word
+                                                                .toLowerCase()
+                                                                .replace(/\s+/g, '')
+                                                                .split('')
+                                                                .sort()
+                                                                .join('');
+    const [first, second] = words.map(normalize)
+    return first === second;
 }
