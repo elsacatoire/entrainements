9f5d7367725b0f8405a2bb8e3310131d80c8975c
Elsa Catoire <elsa.catoire@gmail.com>
2025-02-14 16:08:59 +0100

    initialize Next.js project with TypeScript, Tailwind CSS, and basic configuration files

---
+++ exos_react/training-on/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className="relative min-h-svh">
+      <main className="p-4">
+        Bonjour, je suis une personne très sérieuse.
+      </main>
+      <footer className="absolute inset-x-0 bottom-0 bg-lime-500 text-gray-900 h-12">
+        <div className="p-4">
+          Ceci est le footer
+        </div>
+      </footer>
+    </div>
+  );
+}
