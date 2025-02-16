086d23efeab0d57555f80f1d90d998eb1d51dc2e
Elsa Catoire <elsa.catoire@gmail.com>
2025-02-16 20:04:30 +0100

    refactor CCButton component for improved button sizing and update main container padding in global styles

--- exos_react/training-on/app/components/CCButton.tsx
+++ exos_react/training-on/app/components/CCButton.tsx
@@ -25,7 +25,7 @@ export const Button: React.FC<ButtonProps> = ({
     : "bg-gray-900 hover:hover:bg-gray-800 text-lime-500 border-2 border-lime-600";
 
   const sizeClasses = iconOnly
-    ? "w-10 flex items-center justify-center rounded-xl"
+    ? "min-w-10 flex items-center justify-center rounded-xl"
     : "w-full rounded-md"
 
   return (
