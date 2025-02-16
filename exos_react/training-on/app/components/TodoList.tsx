9f82004089d4d594529f6f3d57e7ec6a84379fa6
Elsa Catoire <elsa.catoire@gmail.com>
2025-02-16 16:09:21 +0100

    refactor TodoList component to add task input functionality and integrate CCButton for task addition

--- exos_react/training-on/app/components/TodoList.tsx
+++ exos_react/training-on/app/components/TodoList.tsx
@@ -1,13 +1,29 @@
 import type React from "react";
 import { useEffect, useState } from "react";
+import { Button } from "./CCButton";
 
 type TodoListProps = {
     children: React.ReactNode
 }
 
+type TaskType = {
+    id: number,
+    title: string,
+    isDone: boolean
+}
+
 export  const TodoList: React.FC<TodoListProps> = ({children}) => {
 
-    const [tasks, setTasks] = useState<{id: number, title: string, isDone: boolean}[]>([]);
+    const [tasks, setTasks] = useState<TaskType[]>([]);
+    const [task, setTask] = useState("");
+
+    const addTask = (e?: React.FormEvent) => {
+        if (e) e.preventDefault();
+        if (task.trim() === "") return;
+        const capitalizedTask = task.charAt(0).toUpperCase() + task.slice(1);
+        setTasks([...tasks, {id: Date.now(), title: capitalizedTask, isDone: false}]);
+        setTask("");
+    }
 
     useEffect(() => {
         setTasks([
@@ -21,21 +37,46 @@ export  const TodoList: React.FC<TodoListProps> = ({children}) => {
         <div className="flex flex-col gap-2 p-2 border-2 border-lime-500 rounded-md">
             <h1 className="text-lg font-semibold">{children}</h1>
 
-            <div>
-                {
-                    tasks.map(t =>{
-                        return (
-                            <div key={t.id} className="flex gap-2">
-                                <input type="checkbox" id={t.title} checked={t.isDone} onChange={() => {
-                                    setTasks(tasks.map(task => 
-                                        task.id === t.id ? { ...task, isDone: !task.isDone } : task
-                                    ));
-                                }}/>
-                                <label htmlFor={t.title}>{t.title}</label>
-                            </div>
-                        )
-                    } )
-                }
+            <div className="flex flex-col gap-2 w-1/2">
+                <form 
+                    className="flex gap-2"
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        addTask();
+                    }}
+                >
+                    <input 
+                        type="text"
+                        id="addTask" 
+                        className="w-full rounded-md border border-lime-500 text-gray-900 p-2"
+                        value={task}
+                        onChange={(e) => setTask(e.target.value)}
+                    />
+                    <div className="max-sm:">
+                        <Button onClick={addTask}>Add</Button>
+                    </div>
+                </form>
+                <div className="p-1">
+                    {
+                        tasks.map(t =>{
+                            return (
+                                <div key={t.id} className="flex gap-2">
+                                    <input 
+                                    type="checkbox" 
+                                    id={t.title} 
+                                    checked={t.isDone} 
+                                    onChange={() => {
+                                        setTasks(tasks.map(task => 
+                                            task.id === t.id ? { ...task, isDone: !task.isDone } : task
+                                        ));
+                                    }}
+                                    />
+                                    <label htmlFor={t.title}>{t.title}</label>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             </div>
         </div>
     )
