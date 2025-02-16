6a2ea2899d7faad0d788baa0f835b4eee534f6bc
Elsa Catoire <elsa.catoire@gmail.com>
2025-02-16 19:19:43 +0100

    refactor Home component layout with updated header and improved styling

--- exos_react/training-on/app/page.tsx
+++ exos_react/training-on/app/page.tsx
@@ -7,9 +7,11 @@ import { TodoList } from "./components/TodoList";
 export default function Home() {
   return (
     <div className="relative min-h-svh bg-gray-900">
-      <main className="p-4">
-        <div className="flex flex-col gap-8">
-          <h1>Bonjour, je suis une personne très sérieuse.</h1>
+      <header className="border-b-2 border-b-lime-200 h-16 flex items-center justify-center bg-gray-800">
+        <h1 className="main-title text-center">Hi! This is a nonsense as I only made it to review my React basis !</h1>
+      </header>
+      <main className="p-8">
+        <div className="flex flex-col gap-8"> 
           <div className="flex flex-col sm:flex-row gap-3 sm:gap-7">
             <Button onClick={() => alert('Clicked!')} variant="primary">Primary</Button>
             <Button onClick={() => alert('Clicked!')} variant="secondary">Secondary</Button>
@@ -19,10 +21,9 @@ export default function Home() {
             <TodoList>What do we need for Apéro?</TodoList>
             <NumberGuess>Guess my age?</NumberGuess>
           </div>
-
         </div>
       </main>
-      <footer className="absolute inset-x-0 bottom-0 bg-lime-500 text-gray-900 h-12">
+      <footer className="absolute inset-x-0 bottom-0 bg-lime-500 text-gray-900 h-12 flex items-center justify-end">
         <div className="p-4">
           Ceci est le footer
         </div>
